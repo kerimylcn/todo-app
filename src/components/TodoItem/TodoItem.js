9295297cf46b3a53aacbd6de894f4e91af09d33b
kerimylcn <kerimylcn@gmail.com>
2021-04-24 16:03:55 +0300

    first commi

---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TodoItem.css";
+
+function Todo(props) {
+  const { content, id } = props;
+  return <div onClick={props.lineText} className="d-flex align-items-center">
+    <span>{content}</span>
+    <button value={id} onClick={props.deleteTodo} className="btn btn-danger ml-auto">Sil 
+    </button>
+    denemeitem
+  </div>;
+}
+
+export default Todo;
